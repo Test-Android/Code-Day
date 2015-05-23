520b43300907cd1a0d6123fad55c4fc3bb6bef24
Jett Kaspar <baseballranger@gmail.com>
2015-05-23 13:03:49 -0500

    This should be working

--- GraySpaceMain.java
+++ GraySpaceMain.java
@@ -1,7 +1,13 @@
+import javax.swing.JFrame;
 
 //made at coday!!
 public class GraySpaceMain 
 {
-
-
+	public static void main(String args[])
+	{
+		JFrame main = new JFrame("This thing");
+		main.setBounds(0,0,500,500);
+		main.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		main.setVisible(true);
+	}
 }
