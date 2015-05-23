b74ef16b78f518f3868c0a4f335dfd90e3b4bd13
Coldparasite <LukeSpringett@gmail.com>
2015-05-23 12:52:47 -0600

    player Fix

--- Player.java
+++ Player.java
@@ -1,5 +1,35 @@
 
 public class Player
 {
-
+	private boolean up,
+					left,
+					right;
+	private boolean alive;
+	private int x,
+				y;
+	private int gravity;
+	
+	public player()
+	{
+		
+	}
+	public update()
+	{
+		
+	}
+	public render()
+	{
+		
+	}
+	public int getX()
+	{
+		int x = 0;
+		return x;
+	}
+	public int getY()
+	{
+		int y = 0;
+		return y;
+	}
+	
 }
