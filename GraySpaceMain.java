fec4daee45456b8658ef3832a379b5dcb4c1468b
Coldparasite <LukeSpringett@gmail.com>
2015-05-23 11:59:25 -0600

    Starting

--- GraySpaceMain.java
+++ GraySpaceMain.java
@@ -1,14 +1,7 @@
-<<<<<<< HEAD
 
+//made at coday!!
 public class GraySpaceMain 
 {
-	public static void main(String args[])
-	{
-		
-	}
-=======
-//made at coday!!
-public class GraySpaceMain {
 
->>>>>>> 1c670945e1b430a387fb85470e1778c13570ad82
+
 }
